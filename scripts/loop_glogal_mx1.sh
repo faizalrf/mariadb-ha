e67d5dbbd4069eef8dab23dc92d7329dec5f1f5e
Faisal Saeed <faisal@mariadb.com>
2021-04-27 22:11:37 +0800

    Fixed the formatting

--- scripts/loop_glogal_mx1.sh
+++ scripts/loop_glogal_mx1.sh
@@ -4,20 +4,22 @@
 # Creates a table and starts inserting and reading values from it outside of a
 # transaction with autocommit=1.
 #
-Port=4601
 maxHost=127.0.0.1
-mariadb -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "CREATE DATABASE IF NOT EXISTS testdb; CREATE OR REPLACE TABLE testdb.tab (id serial, c1 varchar(100), ts timestamp(6));"
+Port=4601
+TableName=tab
+
+mariadb -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "CREATE DATABASE IF NOT EXISTS testdb; CREATE OR REPLACE TABLE testdb.${TableName} (id serial, c1 varchar(100), ts timestamp(6));"
 
 i=1
 while [ $? -eq 0 ]
 do
-    iStmt="INSERT INTO testdb.tab(id, c1) VALUES ($i, CONCAT('Data - ', ROUND(RAND() * 100000, 0)));"
+    iStmt="INSERT INTO testdb.${TableName}(id, c1) VALUES ($i, CONCAT('Data - ', ROUND(RAND() * 100000, 0)));"
     mariadb -N -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "${iStmt}"
     
-    iStmt="SELECT concat('$(tput setaf 7)SELECT on ', @@hostname, '  -  $(tput setaf 3)MaxPort [${Port}]$(tput setaf 7) -> '), 
+    iStmt="SELECT concat('$(tput setaf 7)SELECT FRPM ${TableName} on ', @@hostname, '  -  $(tput setaf 3)MaxPort [${Port}]$(tput setaf 7) ->'), 
                     rpad(coalesce(id, ':(', id), 10, '.'), 
                         IF(COUNT(*)> 0, '$(tput setaf 2)Record Found!$(tput setaf 7)','$(tput setaf 1)! Not Found !$(tput setaf 7)' ) 
-                FROM testdb.tab WHERE id = $i;"
+                FROM testdb.${TableName} WHERE id = $i;"
     output=$(mariadb -N -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "${iStmt}")
     echo ${output}
     i=$((i+1))
--- scripts/loop_mx2.sh
+++ scripts/loop_mx2.sh
@@ -4,17 +4,19 @@
 # Creates a table and starts inserting and reading values from it outside of a
 # transaction with autocommit=1.
 #
-Port=4602
 maxHost=127.0.0.1
-mariadb -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "CREATE DATABASE IF NOT EXISTS testdb; CREATE OR REPLACE TABLE testdb.tab2 (id serial, c1 varchar(100), ts timestamp(6));"
+Port=4602
+TableName=tab2
+
+mariadb -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "CREATE DATABASE IF NOT EXISTS testdb; CREATE OR REPLACE TABLE testdb.${TableName} (id serial, c1 varchar(100), ts timestamp(6));"
 
 i=1
 while [ $? -eq 0 ]
 do
-    echo "INSERT INTO testdb.tab2(c1) VALUES (CONCAT('Data - ', ROUND(RAND() * 100000, 0)));"
-    echo -e "SELECT concat('SELECT on ', @@hostname, ' - MaxPort [${Port}] ->'), rpad(coalesce(id, ':(', id), 10, '.'), 
+    echo "INSERT INTO testdb.${TableName}(c1) VALUES (CONCAT('Data - ', ROUND(RAND() * 100000, 0)));"
+    echo -e "SELECT concat('SELECT FROM ${TableName} on ', @@hostname, ' - MaxPort [${Port}] ->'), rpad(coalesce(id, ':(', id), 10, '.'), 
                 IF(COUNT(*)> 0, '\033[0;32mRecord Found\033[0m','\033[0;31m! Not Found !\033[0m' ) 
-            FROM testdb.tab2 WHERE id = $i;"
+            FROM testdb.${TableName} WHERE id = $i;"
     i=$((i+1))
     sleep 0.05
 done | mariadb -N -u ${UserName} -p${PassWord} -h${maxHost} -P${Port}
