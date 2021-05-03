60920a29f4ea12ac108f773e9552bf8fe7a56a59
Faisal Saeed <faisal@mariadb.com>
2021-05-04 06:23:14 +0800

    Updated Table Name

--- scripts/loop_glogal_mx1.sh
+++ scripts/loop_glogal_mx1.sh
@@ -6,7 +6,7 @@
 #
 maxHost=127.0.0.1
 Port=4601
-TableName=tab
+TableName=tab1
 
 mariadb -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "CREATE DATABASE IF NOT EXISTS testdb; CREATE OR REPLACE TABLE testdb.${TableName} (id serial, c1 varchar(100), ts timestamp(6));"
 
