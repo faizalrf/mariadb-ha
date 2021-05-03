10edfbf4fbbbbb1b6c7b280e96938c3f1411f40d
Faisal Saeed <faisal@mariadb.com>
2021-05-03 09:34:54 +0800

    Adjusted the sleep timing

--- scripts/loop_mx2.sh
+++ scripts/loop_mx2.sh
@@ -14,10 +14,9 @@ i=1
 while [ $? -eq 0 ]
 do
     echo "INSERT INTO testdb.${TableName}(c1) VALUES (CONCAT('Data - ', ROUND(RAND() * 100000, 0)));"
-    echo "SELECT sleep (0.0035) INTO @tmp;"
     echo -e "SELECT concat('SELECT FROM ${TableName} on ', @@hostname, ' - MaxPort [${Port}] ->'), rpad(coalesce(id, ':(', id), 10, '.'), 
                 IF(COUNT(*)> 0, '\033[0;32mRecord Found\033[0m','\033[0;31m! Not Found !\033[0m' ) 
             FROM testdb.${TableName} WHERE id = $i;"
     i=$((i+1))
-    sleep 0.04
+    sleep 0.05
 done | mariadb -N -u ${UserName} -p${PassWord} -h${maxHost} -P${Port}
