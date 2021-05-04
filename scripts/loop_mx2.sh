5b9e2e76b9e7b1c8755ce2649055420f1561047d
Faisal Saeed <faisal@mariadb.com>
2021-05-04 08:22:44 +0800

    Can hit any MaxScale node

--- scripts/loop_mx2.sh
+++ scripts/loop_mx2.sh
@@ -4,8 +4,9 @@
 # Creates a table and starts inserting and reading values from it outside of a
 # transaction with autocommit=1.
 #
+# 4601/4602
 maxHost=127.0.0.1
-Port=4602
+Port=$1
 TableName=tab2
 
 mariadb -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "CREATE DATABASE IF NOT EXISTS testdb; CREATE OR REPLACE TABLE testdb.${TableName} (id serial, c1 varchar(100), ts timestamp(6));"
