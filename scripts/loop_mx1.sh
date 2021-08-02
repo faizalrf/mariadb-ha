bcf6050d5155e2f012153f66c88fb1b96d017e34
Faisal Saeed <faisal@mariadb.com>
2021-08-03 03:45:27 +0800

    Sctript tuning

--- scripts/loop_mx1.sh
+++ scripts/loop_mx1.sh
@@ -4,7 +4,7 @@
 # Creates a table and starts inserting and reading values from it outside of a
 # transaction with autocommit=1.
 #
-# MaxScale port is configured as 4701
+# MaxScale port is configured as 4601
 if [ $# -ne 1 ]; then
     echo "Illegal number of parameters, please specify the port to connect to"
     echo "Verify the Read/Write port within the MaxScale configuration /etc/maxscale.cnf"
@@ -17,13 +17,14 @@ maxHost=127.0.0.1
 Port=$1
 TableName=tab1
 
-mariadb -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "CREATE DATABASE IF NOT EXISTS testdb; CREATE OR REPLACE TABLE testdb.${TableName} (id serial, c1 varchar(100), ts timestamp(6));"
+mariadb -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "DROP DATABASE IF EXISTS testdb; CREATE DATABASE IF NOT EXISTS testdb; CREATE TABLE IF NOT EXISTS testdb.${TableName} (id serial, c1 varchar(100), ts timestamp(6));"
 
 i=1
 while [ $? -eq 0 ]
 do
     echo "INSERT INTO testdb.${TableName}(c1) VALUES (CONCAT('Data - ', ROUND(RAND() * 100000, 0)));"
-    echo -e "SELECT concat('SELECT FROM ${TableName} on ', @@hostname, ' - MaxPort [${Port}] ->'), rpad(coalesce(id, ':(', id), 10, '.'), 
+    echo "UPDATE testdb.${TableName} SET c1 = CONCAT('Data - ', ROUND(RAND() * 100000, 0)) LIMIT 1000;"
+    echo -e "SELECT concat('SELECT FROM ${TableName} on ', @@hostname, ' - MaxPort [${Port}] ->'), rpad($i, 10, '.'), 
                 IF(COUNT(*)> 0, '\033[0;32mRecord Found\033[0m','\033[0;31m! Not Found !\033[0m' ) 
             FROM testdb.${TableName} WHERE id = $i;"
     i=$((i+1))
