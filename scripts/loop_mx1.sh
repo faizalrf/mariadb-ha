311f93daa678cd307e76090de687605a1db3f609
Faisal Saeed <faisal@mariadb.com>
2022-09-09 15:59:17 +0800

    Fixed latency

--- scripts/loop_mx1.sh
+++ scripts/loop_mx1.sh
@@ -17,10 +17,9 @@ i=1
 while [ $? -eq 0 ]
 do
     echo "INSERT INTO ${DBName}.${TableName}(c1) VALUES (CONCAT('Data - ', ROUND(RAND() * 100000, 0)));"
-    echo "UPDATE ${DBName}.${TableName} SET c1 = CONCAT('Data - ', ROUND(RAND() * 100000, 0)) LIMIT 1;"
     echo -e "SELECT concat('SELECT FROM ${TableName} on ', @@hostname, ' - MaxPort [${Port}] ->'), rpad($i, 10, '.'), 
                 IF(COUNT(*)> 0, '\033[0;32mRecord Found\033[0m','\033[0;31m! Not Found !\033[0m' ) 
             FROM ${DBName}.${TableName} WHERE id = $i;"
     i=$((i+1))
-    sleep 0.04
+    sleep 0.03
 done | mariadb -N -u ${UserName} -p${PassWord} -h${maxHost} -P${Port}
